9c67006d763d174e85672b68344d499cfda3a4c0
TamerGabal1 <gabaltamer555@gmail.com>
2024-05-03 16:58:33 -0400

    Fixed profilevc

--- Ithacuts/Controllers/ProfileVC.swift
+++ Ithacuts/Controllers/ProfileVC.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ProfileVC: View {
+    private var profile: Profile = profiles[0]
+    
     var body: some View {
         NavigationStack {
             
             VStack {
                 
-                profilePage
+                profilePage(profile)
                 
                 NavigationLink {
                     EditProfileVC()
@@ -29,57 +31,7 @@ struct ProfileVC: View {
 
 // passing in profilePage(profile) kept saying profile is not in scope so I just commented it all out for now
 
-//private func profilePage(_ profile: Profile) -> some View {
-//    
-//    VStack(alignment: .leading) {
-//        
-//        HStack {
-//            
-//            Image(systemName: "xbox.logo")
-//                .resizable()
-//                .frame(width: 100, height: 100)
-//                .padding(.leading, 25)
-//                .padding(.trailing, 15)
-//            
-//            VStack(alignment: .leading) {
-//                
-//                Text("Hi,")
-//                    .font(.largeTitle)
-//                    .fontWeight(.semibold)
-//                
-//                Text(profile.username)
-//                    .font(.largeTitle)
-//                    .fontWeight(.semibold)
-//                
-//            }
-//            
-//            Spacer()
-//        }
-//        
-//        Text("Age: \(profile.age)")
-//            .font(.title2)
-//            .fontWeight(.medium)
-//            .padding(.top, 10)
-//            .padding(.leading, 30)
-//        
-//        Text("Preference: \(profile.preference)")
-//            .font(.title2)
-//            .fontWeight(.medium)
-//            .padding(.top, 5)
-//            .padding(.leading, 30)
-//        
-//        Text("Hair Style: \(profile.hairStyle)")
-//            .font(.title2)
-//            .fontWeight(.medium)
-//            .padding(.top, 5)
-//            .padding(.leading, 30)
-//
-//        Spacer()
-//        
-//    }
-//}
-
-private var profilePage: some View {
+private func profilePage(_ profile: Profile) -> some View {
     
     VStack(alignment: .leading) {
         
@@ -97,7 +49,7 @@ private var profilePage: some View {
                     .font(.largeTitle)
                     .fontWeight(.semibold)
                 
-                Text("User Name") // make the username reflect whatever was typed in the username text field
+                Text(profile.username)
                     .font(.largeTitle)
                     .fontWeight(.semibold)
                 
@@ -106,19 +58,19 @@ private var profilePage: some View {
             Spacer()
         }
         
-        Text("Age: ")
+        Text("Age: \(profile.age)")
             .font(.title2)
             .fontWeight(.medium)
             .padding(.top, 10)
             .padding(.leading, 30)
         
-        Text("Preference: ")
+        Text("Preference: \(profile.preference)")
             .font(.title2)
             .fontWeight(.medium)
             .padding(.top, 5)
             .padding(.leading, 30)
         
-        Text("Hair Style: ")
+        Text("Hair Style: \(profile.hairStyle)")
             .font(.title2)
             .fontWeight(.medium)
             .padding(.top, 5)
@@ -128,6 +80,56 @@ private var profilePage: some View {
         
     }
 }
+
+//private var profilePage: some View {
+//    
+//    VStack(alignment: .leading) {
+//        
+//        HStack {
+//            
+//            Image(systemName: "xbox.logo")
+//                .resizable()
+//                .frame(width: 100, height: 100)
+//                .padding(.leading, 25)
+//                .padding(.trailing, 15)
+//            
+//            VStack(alignment: .leading) {
+//                
+//                Text("Hi,")
+//                    .font(.largeTitle)
+//                    .fontWeight(.semibold)
+//                
+//                Text("User Name") // make the username reflect whatever was typed in the username text field
+//                    .font(.largeTitle)
+//                    .fontWeight(.semibold)
+//                
+//            }
+//            
+//            Spacer()
+//        }
+//        
+//        Text("Age: ")
+//            .font(.title2)
+//            .fontWeight(.medium)
+//            .padding(.top, 10)
+//            .padding(.leading, 30)
+//        
+//        Text("Preference: ")
+//            .font(.title2)
+//            .fontWeight(.medium)
+//            .padding(.top, 5)
+//            .padding(.leading, 30)
+//        
+//        Text("Hair Style: ")
+//            .font(.title2)
+//            .fontWeight(.medium)
+//            .padding(.top, 5)
+//            .padding(.leading, 30)
+//
+//        Spacer()
+//        
+//    }
+//}
 
 private var profilePhotos: some View {
     
